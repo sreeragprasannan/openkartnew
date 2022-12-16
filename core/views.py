d3dd216ff52c5dd317d7600079ba38ec341633f4
sreerag <sreeraaag@gmail.com>
2022-12-16 12:27:30 +0530

    frontpage and shoppage

--- core/views.py
+++ core/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from product.models import Product, Category
 # Create your views here.
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    products = Product.objects.all()[0:8]
+    
+    return render(request, 'core/frontpage.html', {'products': products})
+
+def shop(request):
+    catagories = Category.objects.all()
+    products = Product.objects.all()
+    
+    active_catagory = request.GET.get('catagory', '')
+    context ={
+        'catagories': catagories,
+        'products': products,
+        'active_catagory':active_catagory,
+    }
+        
+    return render(request, 'core/shop.html', context)
+    
