21a0e89e88af542d497e6294836b5884c55270b0
sreerag <sreeraaag@gmail.com>
2022-12-26 20:05:02 +0530

    try to display oder in myaccount

--- openkart/urls.py
+++ openkart/urls.py
@@ -8,7 +8,8 @@ from django.urls import path,include
 urlpatterns = [
     path('', include('core.urls')),
     path('cart/',include('cart.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
+    path('order/', include('order.urls')),
+    path("__reload__/", include('django_browser_reload.urls')),
     path('admin/', admin.site.urls),
 ] 
 
--- order/models.py
+++ order/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from product.models import Product
-
+from itertools import product
 # Create your models here.
 class Order(models.Model):
     ORDERED = 'odered'
