9053c8b3a116cce03d9ae6d148504dda2a98858c
sreerag <sreeraaag@gmail.com>
2023-01-19 11:04:35 +0530

    rating and comments

--- product/models.py
+++ product/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.files import File
-
+from django.contrib.auth.models import User
 from io import BytesIO
 from PIL import Image
 
@@ -55,3 +55,20 @@ class Product(models.Model):
 
         return thumbnail
     
+    def get_rating(self):
+        reviews_total = 0
+        
+        for review in self.reviews.all():
+            reviews_total += review.rating
+            
+        if reviews_total > 0:
+            return reviews_total / self.reviews.count()
+        
+        return 0
+
+class Review(models.Model):
+    product = models.ForeignKey(Product,related_name='reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField(default=3)
+    content = models.TextField()
+    created_by = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
